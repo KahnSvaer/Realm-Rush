ceea1d42809cc83029c7bbc99693d4a418600149
Shivansh Pachnanda <Shivansh.pachnanda1@gmail.com>
2024-01-23 00:27:56 +0530

    Added initial Mouse Inputs

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> waypoints;
+    [SerializeField] [Range(0f, 5f)]float speed = 1f;
+
     private void Start() {
         StartCoroutine(DisplayWaypoints());
     }
@@ -13,8 +15,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in waypoints)
         {
-            yield return new WaitForSeconds(1f);
-            transform.position = waypoint.transform.position;
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercentage = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercentage < 1)
+            {
+                travelPercentage += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercentage);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,18 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
+    [SerializeField] bool isPlaceable;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown() 
     {
-        
+        if (isPlaceable)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
