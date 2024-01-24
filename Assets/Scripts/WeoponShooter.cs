ed3f8e8c06a0b1b1e0c150773c64f46b233cd4ee
Shivansh Pachnanda <Shivansh.pachnanda1@gmail.com>
2024-01-24 23:28:59 +0530

    Updated the shooting system

--- Assets/Scripts/WeoponShooter.cs
+++ Assets/Scripts/WeoponShooter.cs
@@ -7,18 +7,60 @@ public class WeoponShooter : MonoBehaviour
     [SerializeField] Transform Aimer;
     [SerializeField] float weoponDamage = 1f;
     public float WeoponDamage {get {return weoponDamage; }}
+    [SerializeField] float ballistaRange = 20f;
+
+    ParticleSystem boltParticle;
 
     Transform target;
-
-    private void Start() 
-    {
-        target = FindObjectOfType<EnemyMover>().transform; //This needs to change later   
+    
+    private void Start() {
+        boltParticle = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        SetNFireTarget();
         AimTarget();
+    }
+
+    private void SetNFireTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        target = FindClosestEnemy(enemies);
+        SetAmmoParticles(enemies.Length);
+    }
+
+    private Transform FindClosestEnemy(Enemy[] enemies)
+    {
+        Transform closestEnemy = null;
+        float closestDistance = Mathf.Infinity;
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            float enemyDistance = Vector3.Distance(enemies[i].transform.position, transform.position);
+            if (enemyDistance < closestDistance)
+            {
+                closestDistance = enemyDistance;
+                closestEnemy = enemies[i].transform;
+            }
+        }
+        return closestEnemy;
+    }
+
+    private void SetAmmoParticles(int Length)
+    {
+        if (Length == 0)
+        {
+            boltParticle.Stop();
+        }
+        else if (Vector3.Magnitude(target.position-transform.position)>ballistaRange)
+        {
+            boltParticle.Stop();
+        }
+        else if (!boltParticle.isPlaying)
+        {
+            boltParticle.Play();
+        }
     }
 
     private void AimTarget()
